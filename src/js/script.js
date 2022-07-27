4d7ce910f9b9f5c33b7f28fb288cb3993f60c5f8
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-27 22:46:50 +0200

    Add for...in loop

--- src/js/script.js
+++ src/js/script.js
@@ -66,8 +66,9 @@
 
       console.log('thisApp.data:', thisApp.data);
 
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct);
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
     },
     initData: function(){
       const thisApp = this;
