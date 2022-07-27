3f6c0c99920cecd1e760509f3abee9ba766f7b5c
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-27 22:24:46 +0200

    Add app.initMenu instance

--- src/js/script.js
+++ src/js/script.js
@@ -61,6 +61,10 @@
   }
 
   const app = {
+    initMenu: function() {
+      const testProduct = new Product();
+      console.log('testProduct:', testProduct);
+    },
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -68,8 +72,9 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initMenu();
     },
   };
-
   app.init();
 }
