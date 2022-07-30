77446f00c86fdb7f3b6048f3a3658f886064136e
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-30 17:31:56 +0200

    Opt 1 and 3 working

--- src/js/script.js
+++ src/js/script.js
@@ -145,18 +145,37 @@
       for(let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
+        // console.log(paramId, param);
 
         // for every option in this category
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
+          const selected = formData[paramId].includes(optionId);
+          // console.log(optionId, option);
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+            // check if the option is not default
+            if((option.default !== true) && (selected === true)) {
+              // add option price to price variable
+              console.log('opt1');
+              price += option.price;
+            } else {
+              if (((option.default === true) && (selected === false)) || ((option.default !== true) && (selected === false)) ) {
+              // check if the option is default
+              // reduce price variable
+                console.log('opt2');
+                price -= option.price;
+              } else {
+                console.log('opt3');
+                price;
+              }
+            }
+          }
         }
+        // update calculated price in the HTML
+        thisProduct.priceElem.innerHTML = price;
       }
-
-      // update calculated price in the HTML
-      thisProduct.priceElem.innerHTML = price;
     }
   }
 
