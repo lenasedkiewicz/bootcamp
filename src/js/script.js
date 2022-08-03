412c84fc89a8ebb43a4d337786b13fae9b7af107
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2022-08-03 15:17:54 +0200

    Add  initCart function and call in const app

--- src/js/script.js
+++ src/js/script.js
@@ -319,6 +319,12 @@
 
       thisApp.data = dataSource;
     },
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
     init: function(){
       const thisApp = this;
       // console.log('*** App starting ***');
@@ -329,6 +335,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
   app.init();
