0fe3c4ee9ad9d905c708b7eed44683a07efbb32c
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2022-09-07 20:31:54 +0200

    Add styles to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/normalize.scss';
+import './styles/global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <h1>HEllo world</h1>
   </div>
 );
