00d6f72f7861a4b58621579fab200b847146a628
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-15 20:04:06 +0200

    Rewrite App.js component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import BooksList from "./components/BooksList/BooksList";
 import BookForm from "./components/BookForm/BookForm";
-import { useState } from "react";
+import React, { useState } from "react";
 import shortid from "shortid";
 
-const App = () => {
-  const [books, setBooks] = useState([
-    { id: 1, title: "Shogun", author: "James Clavell" },
-    { id: 2, title: "the Witcher", author: "Andrzej Sapkowski" },
-  ]);
-
-  const removeBook = (bookId) => {
-    setBooks(books.filter((book) => book.id !== bookId));
+class App extends React.Component {
+  state = {
+    books: [
+      { id: 1, title: "Shogun", author: "James Clavell" },
+      { id: 2, title: "the Witcher", author: "Andrzej Sapkowski" },
+    ],
   };
 
-  const addBook = (newBook) => {
-    setBooks([
-      ...books,
-      { id: shortid(), title: newBook.title, author: newBook.author },
-    ]);
+  removeBook = (bookId) => {
+    this.setState({
+      books: this.state.books.filter((book) => book.id !== bookId),
+    });
   };
 
-  return (
-    <div>
-      <h1>Books App</h1>
-      <BooksList books={books} removeBook={removeBook} />
-      <BookForm addBook={addBook} />
-    </div>
-  );
-};
+  addBook = (newBook) => {
+    this.setState({
+      books: [
+        ...this.state.books,
+        { id: shortid(), title: newBook.title, author: newBook.author },
+      ],
+    });
+  };
+
+  render() {
+    const { books } = this.state;
+    const { addBook, removeBook } = this;
+
+    return (
+      <div>
+        <h1>Books App</h1>
+        <BooksList books={books} removeBook={removeBook} />
+        <BookForm addBook={addBook} />
+      </div>
+    );
+  }
+}
 
 export default App;
