d6deba6cb5ccf47eacfed9d727999345548ba3cf
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-15 17:16:59 +0200

    Create data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import BooksList from "./components/BooksList/BooksList";
 import BookForm from "./components/BookForm/BookForm";
+import { useState } from "react";
 
 const App = () => {
+  const [books, setBooks] = useState([
+    { id: 1, title: "Shogun", author: "James Clavell" },
+    { id: 2, title: "the Witcher", author: "Andrzej Sapkowski" },
+  ]);
+
   return (
     <div>
       <h1>Books App</h1>
@@ -9,6 +15,6 @@ const App = () => {
       <BookForm />
     </div>
   );
-}
+};
 
 export default App;
