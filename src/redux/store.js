a89f4086a2c9d194e4c7f2486da5b77a470c4fcc
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-16 18:46:00 +0200

    Add data to store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,20 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
 const reducer = (state, action) => {
-    return state;
-}
+  return state;
+};
 
-const initialState = {};
+const initialState = {
+  books: [
+    { id: 1, title: "Shogun", author: "James Clavell" },
+    { id: 2, title: "the Witcher", author: "Andrzej Sapkowski" },
+  ],
+};
 
 const store = createStore(
-    reducer,
-    initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
