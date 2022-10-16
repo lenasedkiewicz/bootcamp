37ae20c74060b6544ca19897173b5aaefb4c3626
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-10-16 18:46:24 +0200

    Move code from App to BooksList component

--- src/components/BooksList/BooksList.js
+++ src/components/BooksList/BooksList.js
@@ -1,4 +1,17 @@
-const BooksList = ({ books, removeBook }) => {
+import shortid from "shortid";
+
+const BooksList = ({ books }) => {
+
+    const removeBook = (bookId) => {
+        setBooks(books.filter((book) => book.id !== bookId));
+      };
+    
+      const addBook = (newBook) => {
+        setBooks([
+          ...books,
+          { id: shortid(), title: newBook.title, author: newBook.author },
+        ]);
+      };
     return (
         <ul>
             {books.map(book => <li key={book.id}>{book.title} by {book.author} <button onClick={() => removeBook(book.id)}>Remove</button></li>)}
