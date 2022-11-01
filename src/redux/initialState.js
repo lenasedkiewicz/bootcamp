758d0c64f8983212a5742bdaff7b028ff47813b5
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-11-01 15:19:25 +0100

    Add exemplary posts in initialState

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,5 +1,38 @@
 const initialState = {
-    posts: [],
+  posts: [
+    {
+      id: "1",
+      title: "Article title",
+      shortDescription: "Short description of the article...",
+      content: "Main content of the article",
+      publishedDate: "01-02-2022",
+      author: "John Doe",
+    },
+    {
+      id: "2",
+      title: "Snother Article title",
+      shortDescription: "Short description is short, because...",
+      content: "Main article of the content",
+      publishedDate: "02-02-2022",
+      author: "Jane Doe",
+    },
+    {
+      id: "3",
+      title: "Yet Another Article title",
+      shortDescription: "Short description of the short article!",
+      content: "Main content of the content",
+      publishedDate: "03-02-2022",
+      author: "Jamie Doe",
+    },
+    {
+      id: "4",
+      title: "And Another Article title",
+      shortDescription: "Short article with short description.",
+      content: "Main article of the article",
+      publishedDate: "04-02-2022",
+      author: "Janett Doe",
+    },
+  ],
 };
 
 export default initialState;
