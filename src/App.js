9110011e595a7c84083a5c56b7c25608951b3d01
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-11-01 13:38:13 +0100

    Add Header, NavBar and Footer

--- src/App.js
+++ src/App.js
@@ -5,17 +5,24 @@ import PostAdd from "./pages/PostAdd/PostAdd";
 import PostEdit from "./pages/PostEdit/PostEdit";
 import About from "./pages/About/About";
 import NotFound from "./pages/NotFound/NotFound";
+import Header from "./views/Header/Header";
+import Footer from "./views/Footer/Footer";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/post/:id" element={<SinglePost />} />
-      <Route path="/post/add" element={<PostAdd />} />
-      <Route path="/post/edit/:id" element={<PostEdit />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Container>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post/:id" element={<SinglePost />} />
+        <Route path="/post/add" element={<PostAdd />} />
+        <Route path="/post/edit/:id" element={<PostEdit />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Container>
   );
 }
 
---
+++ src/views/Header/Header.js
@@ -0,0 +1,12 @@
+import { Navbar } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Header</h1>
+    </>
+  );
+};
+
+export default Header;
