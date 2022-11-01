20fe639639398bf7765ec9a9a22ebaf79835150b
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-11-01 22:46:49 +0100

    Add selector getPostById

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -1,14 +1,17 @@
 //selectors
 export const getAllPosts = (state) => state.posts;
+export const getPostById = ({ posts }, id) =>
+  posts.find((post) => post.id === id);
+
 // actions
-const createActionName = actionName => `app/posts/${actionName}`;
+const createActionName = (actionName) => `app/posts/${actionName}`;
 
 // action creators
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
     default:
       return statePart;
-  };
+  }
 };
 
 export default postsReducer;
