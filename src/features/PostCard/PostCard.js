6b9deb575a651bafb6d6842229058a78ec665fdc
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-11-01 18:22:03 +0100

    Approach to add card

---
+++ src/features/PostCard/PostCard.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { Card, Button } from "react-bootstrap";
+
+const PostCard = (props) => {
+  return (
+    <>
+      <Card style={{ width: "18rem" }} className="mb-4">
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <p className="font-weight-bold">Author: {props.author}</p>
+          <p className="text-muted">Published: {props.date}</p>
+          <Card.Text>
+            <p className="font-weight-bold">
+              Short description: {props.description}
+            </p>
+          </Card.Text>
+          <Link key={props.postId} to={"/post/" + props.postId}>
+            <Button variant="primary">Read more</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default PostCard;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,30 @@
+import PostCard from "../../features/PostCard/PostCard";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getAllPosts } from "../../redux/postsRedux";
+
 const Home = () => {
-    return ( <h1> All posts</h1>)
-}
+  const posts = useSelector((state) => getAllPosts);
+  return (
+    <>
+      <div className="d-flex justify-content-between">
+        <h1>All posts</h1>
+        <Link to="/post/add">
+          <Button variant="outline-info">Add post</Button>
+        </Link>
+        {posts.map(post => (
+          <PostCard
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            date={post.publishedDate}
+            description={post.shortDescription}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Home;
