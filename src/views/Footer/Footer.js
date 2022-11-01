2796f381b11f180c7f57b97fd2cb84a2875d9abc
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-11-01 14:54:49 +0100

    Add Footer styling and content

--- src/views/Footer/Footer.js
+++ src/views/Footer/Footer.js
@@ -1,5 +1,7 @@
 const Footer = () => {
-  return <h1>Footer</h1>;
-};
+  return (
+       <div className="text-center text-muted" >Copyright &copy; Blog.app 2022</div>
+  );
+}
 
 export default Footer;
