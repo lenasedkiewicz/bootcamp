46fafc127cb14361b33903ee5f68364f1e5a505d
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-11-10 23:45:57 +0100

    add semi colon

--- src/features/AddPostForm/AddPostForm.js
+++ src/features/AddPostForm/AddPostForm.js
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addPost } from "../../redux/postsRedux";
-import PostForm from "../PostForm/PostForm"
+import PostForm from "../PostForm/PostForm";
 
 const AddPostForm = () => {
   const navigate = useNavigate();
