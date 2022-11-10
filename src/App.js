6d9853ad0993ecb2b001099558b2c6c63c93ef42
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-11-11 00:00:20 +0100

    Add routing in App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import SinglePost from "./pages/SinglePost/SinglePost";
-import PostAdd from "./pages/PostAdd/PostAdd";
-import PostEdit from "./pages/PostEdit/PostEdit";
 import About from "./pages/About/About";
 import NotFound from "./pages/NotFound/NotFound";
 import Header from "./views/Header/Header";
 import Footer from "./views/Footer/Footer";
 import { Container } from "react-bootstrap";
+import AddPostForm from './features/AddPostForm/AddPostForm';
+import EditPostForm from './features/EditPostForm/EditPostForm';
 
 function App() {
   return (
@@ -16,8 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:id" element={<SinglePost />} />
-        <Route path="/post/add" element={<PostAdd />} />
-        <Route path="/post/edit/:id" element={<PostEdit />} />
+        <Route path="/post/add" element={<AddPostForm />} />
+        <Route path="/post/edit/:postId" element={<EditPostForm />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
