8a2306fa935b5716ff6232af739dc5d963e04cb5
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-11-15 18:23:07 +0100

    Add renderBookList function

--- src/js/script.js
+++ src/js/script.js
@@ -18,4 +18,23 @@
       document.querySelector(select.templateOf.bookTemplate).innerHTML
     ),
   };
+
+  const renderBookList = function () {
+    for (const book of dataSource.books) {
+
+      //generate HTML based on Handlebars template
+      const generatedHTML = templates.bookCard(book);
+
+      //create book object using utils
+      const singleBook = utils.createDOMFromHTML(generatedHTML);
+
+      //find books-list class in DOM
+      const listOfBooks = document.querySelector(select.books.bookList);
+
+      //add book to list
+      listOfBooks.appendChild(singleBook);
+    }
+  };
+
+  renderBookList();
 }
