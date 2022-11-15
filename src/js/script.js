53dd3c75e4b992072f00f044feb627bba616ea9b
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-11-15 18:22:09 +0100

    Add references

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
+
+{
+  ("use strict");
+
+  const select = {
+    books: {
+      booksPanel: ".books-panel",
+      bookList: ".books-list",
+    },
+    templateOf: {
+      bookTemplate: "#template-book",
+    },
+  };
+}
