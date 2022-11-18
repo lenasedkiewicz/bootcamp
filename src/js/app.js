6c2f4ca12197291d182e8ef76a1da3c51306bbee
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-11-18 22:18:56 +0100

    Code refactoring

--- src/js/app.js
+++ src/js/app.js
@@ -1,56 +1,50 @@
-/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
-
 // const { stringify } = require('postcss');
 
-{
-  'use strict';
+const app = {
+  initMenu: function() {
+    const thisApp = this;
 
-  const app = {
-    initMenu: function() {
-      const thisApp = this;
+    // console.log('thisApp.data:', thisApp.data);
 
-      // console.log('thisApp.data:', thisApp.data);
+    for(let productData in thisApp.data.products){
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+  },
+  initData: function(){
+    const thisApp = this;
 
-      for(let productData in thisApp.data.products){
-        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
-      }
-    },
-    initData: function(){
-      const thisApp = this;
+    thisApp.data = {};
+    const url = settings.db.url + '/' + settings.db.products;
+    fetch(url)
+      .then(function(rawResponse){
+        return rawResponse.json();
+      })
+      .then(function(parsedResponse){
+        // console.log('parsedResponse: ', parsedResponse);
+        /* save parsedResponse at thisApp.data.products */
+        thisApp.data.products = parsedResponse;
+        /* execute initMenu method */
+        app.initMenu();
 
-      thisApp.data = {};
-      const url = settings.db.url + '/' + settings.db.products;
-      fetch(url)
-        .then(function(rawResponse){
-          return rawResponse.json();
-        })
-        .then(function(parsedResponse){
-          // console.log('parsedResponse: ', parsedResponse);
-          /* save parsedResponse at thisApp.data.products */
-          thisApp.data.products = parsedResponse;
-          /* execute initMenu method */
-          app.initMenu();
+      });
+    // console.log('thisApp.data', JSON.stringify(thisApp.data));
+  },
+  initCart: function(){
+    const thisApp = this;
 
-        });
-      // console.log('thisApp.data', JSON.stringify(thisApp.data));
-    },
-    initCart: function(){
-      const thisApp = this;
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
+  },
+  init: function(){
+    const thisApp = this;
+    // console.log('*** App starting ***');
+    // console.log('thisApp:', thisApp);
+    // console.log('classNames:', classNames);
+    // console.log('settings:', settings);
+    // console.log('templates:', templates);
 
-      const cartElem = document.querySelector(select.containerOf.cart);
-      thisApp.cart = new Cart(cartElem);
-    },
-    init: function(){
-      const thisApp = this;
-      // console.log('*** App starting ***');
-      // console.log('thisApp:', thisApp);
-      // console.log('classNames:', classNames);
-      // console.log('settings:', settings);
-      // console.log('templates:', templates);
-
-      thisApp.initData();
-      thisApp.initCart();
-    },
-  };
-  app.init();
-}
+    thisApp.initData();
+    thisApp.initCart();
+  },
+};
+app.init();
