cb703f4d6a04f94a9fd7eba4add8426f75a18194
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-11-22 18:22:31 +0100

    Add Booking code

--- src/js/Components/Booking.js
+++ src/js/Components/Booking.js
@@ -1,5 +1,6 @@
 import {select, templates} from '../settings.js';
 import utils from '../utils.js';
+import AmountWidget from './AmountWidget.js';
 
 
 class Booking {
@@ -12,26 +13,30 @@ class Booking {
 
   render(element){
     const thisBooking = this;
-
     thisBooking.dom = {};
     thisBooking.dom.wrapper = element;
     thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
     thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
-
     /* generate HTML based on template */
-    const generatedHTML = templates.bookingWidget(element);
+    const generatedHTML = templates.bookingWidget();
     /* create element using utils.createElementFromHTML */
     thisBooking.element = utils.createDOMFromHTML(generatedHTML);
     /* find menu container */
     const bookingContainer = document.querySelector(select.containerOf.booking);
     /* add element to menu */
     bookingContainer.appendChild(thisBooking.element).innerHTML;
-
   }
 
   initWidgets(){
+    const thisBooking = this;
+    thisBooking.amountWidget = new AmountWidget(select.widgets.amount);
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+
+    thisBooking.dom.hoursAmount.addEventListener('updated', function(){
+      thisBooking.amount = thisBooking.amountWidget.value;
+    });
   }
 }
-
 
 export default Booking;
