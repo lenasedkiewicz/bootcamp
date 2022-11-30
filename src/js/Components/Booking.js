f7eaaf58ad6ad92eb2dc6234ad5a17770290f165
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-11-30 21:45:48 +0100

    Fix url data display error

--- src/js/Components/Booking.js
+++ src/js/Components/Booking.js
@@ -36,11 +36,11 @@ class Booking {
     };
 
     const urls = {
-      booking:       settings.db.url + '/' + settings.db.booking
+      booking:       settings.db.url + '/' + settings.db.bookings
                      + '?' + params.booking.join('&'),
-      eventsCurrent: settings.db.url + '/' + settings.db.event
+      eventsCurrent: settings.db.url + '/' + settings.db.events
                      + '?' + params.eventsCurrent.join('&'),
-      eventsRepeat:  settings.db.url + '/' + settings.db.event
+      eventsRepeat:  settings.db.url + '/' + settings.db.events
                      + '?' + params.eventsRepeat.join('&'),
     };
 
