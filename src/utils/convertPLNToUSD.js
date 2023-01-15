aa322f76c6349622ad9cd47b08ec1bd2dc579939
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-15 18:59:54 +0100

    Force converter to accept only numbers

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,7 +1,13 @@
 export const convertPLNToUSD = (PLN) => {
 
+  if (typeof(PLN) === 'string' || PLN === undefined) {
+    return NaN;
+  } else if (typeof(PLN) !== 'number' && typeof(PLN) !== 'string') {
+    return 'Expected number, received other type of input!';
+  }
+
   const PLNtoUSD = PLN / 3.5;
-  
+
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
