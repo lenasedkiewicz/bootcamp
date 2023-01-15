25e5ee4b66b14e40c46ebe002c5d5c0f37f6e6f8
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-15 16:41:59 +0100

    Add Quill post editor

--- src/features/PostForm/PostForm.js
+++ src/features/PostForm/PostForm.js
@@ -1,6 +1,8 @@
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { useState } from "react";
 import dateFormat from "dateformat";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const PostForm = ({ action, actionText, ...props }) => {
   const [title, setTitle] = useState(props.title || "");
@@ -76,18 +78,7 @@ const PostForm = ({ action, actionText, ...props }) => {
               />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="content">
-              <Form.Label>Main Content</Form.Label>
-              <Form.Control
-                type="text"
-                as="textarea"
-                rows={10}
-                placeholder="Add your blog post text here."
-                required
-                onChange={(e) => setContent(e.target.value)}
-                value={content}
-              />
-            </Form.Group>
+            <ReactQuill value={content} onChange={setContent} />
 
             <Button variant="primary" type="submit">
               {actionText}
