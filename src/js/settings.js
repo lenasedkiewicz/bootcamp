1d3cc55a3538bf351511d0e7a5a1eda7d309cd1f
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-17 21:54:33 +0100

    Add settings.js file

---
+++ src/js/settings.js
@@ -0,0 +1,36 @@
+export const select = {
+  templateOf: {
+    aboutPage: '#template-about-page',
+    finderPage: '#template-finder-page',
+  },
+  containerOf: {
+    pages: '#pages',
+    aboutPage: '.about-page-wrapper',
+    finderPage: '.finder-page-wrapper',
+  },
+  nav: {
+    links: '.navigation-bar a',
+  },
+};
+
+export const classNames = {
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  },
+};
+
+export const settings = {};
+
+export const templates = {
+  aboutPage: Handlebars.compile(
+    document.querySelector(select.templateOf.aboutPage).innerHTML
+  ),
+  finderPage: Handlebars.compile(
+    document.querySelector(select.templateOf.finderPage).innerHTML
+  ),
+};
+
+export default settings;
