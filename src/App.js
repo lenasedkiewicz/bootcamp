a67724696ab6c4e8b6ec2f1e6bff6e83867dc834
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-28 15:39:00 +0100

    Change fetchBooks placement

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import BooksList from "./components/BooksList/BooksList";
 import BookForm from "./components/BookForm/BookForm";
-import { updateBooks } from "./redux/booksRedux";
+import { fetchBooks } from "./redux/booksRedux";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 
 const App = () => {
   const dispatch = useDispatch();
-  const fetchBooks = () => {
-    fetch("http://localhost:3131/api/books")
-      .then((res) => res.json())
-      .then((books) => dispatch(updateBooks(books)));
-  };
 
-  useEffect(fetchBooks, [dispatch]);
+  useEffect(() => fetchBooks(dispatch), [dispatch]);
   return (
     <div>
       <h1>Books App</h1>
--- src/redux/booksRedux.js
+++ src/redux/booksRedux.js
@@ -13,6 +13,11 @@ const UPDATE_BOOKS = createActionName("UPDATE_BOOKS");
 export const removeBook = (payload) => ({ type: REMOVE_BOOK, payload });
 export const addBook = (payload) => ({ type: ADD_BOOK, payload });
 export const updateBooks = (payload) => ({ type: UPDATE_BOOKS, payload });
+export const fetchBooks = (dispatch) => {
+  fetch("http://localhost:3131/api/books")
+    .then((res) => res.json())
+    .then((books) => dispatch(updateBooks(books)));
+};
 
 const reducer = (statePart = [], action) => {
   switch (action.type) {
