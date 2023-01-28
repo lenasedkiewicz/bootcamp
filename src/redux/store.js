f3a7a372b4e2f73c5cb439a665e26c82cc9bda48
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-28 15:41:28 +0100

    Add code for thunk usage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,21 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import initialState from "./initialState";
 import booksReducer from "./booksRedux";
 
 const reducer = combineReducers({
-  books: booksReducer
-})
+  books: booksReducer,
+});
 
 const store = createStore(
   reducer,
   initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
+  )
 );
 
 export default store;
