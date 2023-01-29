e9941dc59f694c806b40ed2b57c6b07b2ee570d1
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-29 21:15:51 +0100

    Fix book adding

--- src/redux/booksRedux.js
+++ src/redux/booksRedux.js
@@ -25,7 +25,7 @@ export const addBookRequest = (newBook) => {
     const options = {
       method: 'POST',
       headers: {
-        'ContentType': 'application/json'
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(newBook),
     };
