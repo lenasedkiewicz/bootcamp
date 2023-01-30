ef23ab7ea38997c98762720c5813ae2de89c1d59
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-30 22:58:10 +0100

    Add EditTable

---
+++ src/components/features/EditTable.js
@@ -0,0 +1,23 @@
+import { useParams, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getTableById } from "../../redux/tablesRedux";
+import TableForm from "./TableForm";
+
+function EditTable() {
+  const { tableId } = useParams();
+  const tableData = useSelector((state) => getTableById(state, tableId));
+
+  if (!tableData) return <Navigate to="/" />;
+  return (
+    <TableForm
+      actionText="Change"
+      id={tableData.id}
+      people={tableData.people}
+      maxPeople={tableData.maxPeople}
+      status={tableData.status}
+      bill={tableData.bill}
+    />
+  );
+}
+
+export default EditTable;
