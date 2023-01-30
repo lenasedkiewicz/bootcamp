c37c17bb3f2ca668d445a509f521171a379fd501
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-30 22:59:40 +0100

    Edit App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-const App = () => {
+import Header from "./components/common/Header";
+import Home from "./components/pages/Home";
+import EditTable from './components/features/EditTable';
+import NotFound from './components/pages/NotFound';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchTables } from './redux/tablesRedux';
+import { fetchStatus } from './redux/statusRedux';
+
+import { Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router';
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(fetchTables()), [dispatch]);
+  useEffect(() => dispatch(fetchStatus()), [dispatch]);
+
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <main>
+      <Container>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/table/:tableId" element={<EditTable />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
+    </main>
   );
 }
 
