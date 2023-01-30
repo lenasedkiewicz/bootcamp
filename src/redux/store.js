2a9f2761b9122c9142aeb37e2be9663850c0f64f
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-30 21:36:04 +0100

    Add Redux code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,21 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import initialState from './initialState';
 import tablesReducer from './tablesRedux';
+import statusReducer from './statusRedux';
+
 
 const subreducers = {
-    tables: tablesReducer,
-}
+  tables: tablesReducer,
+  status: statusReducer
+};
 
 const reducer = combineReducers(subreducers);
+
 const store = createStore(
   reducer,
-  initialState,
-
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
   )
 );
 
--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -1,13 +1,54 @@
+import { API_URL } from "../config.js";
+
 //selectors
+export const getTableById = ({ tables }, tableId) =>
+  tables.find((table) => table.id === tableId);
+export const getAllTables = ({ tables }) => tables;
 
 // actions
-const createActionName = actionName => `app/tables/${actionName}`;
+const createActionName = (actionName) => `app/tables/${actionName}`;
+export const updateTable = (payload) => ({ type: UPDATE_TABLE, payload });
+export const editTable = (payload) => ({ type: EDIT_TABLE, payload });
 
 // action creators
-const tablesReducer = (statePart = [], action) => {
-  switch (action.type) {
-    default:
-      return statePart;
+const UPDATE_TABLE = createActionName("UPDATE_TABLE");
+const EDIT_TABLE = createActionName("EDIT_TABLE");
+
+export const fetchTables = () => {
+  return (dispatch) => {
+    fetch(API_URL + "/tables")
+      .then((res) => res.json())
+      .then((tables) => dispatch(updateTable(tables)));
   };
 };
+
+export const updateTableRequest = (updatedTable) => {
+  return (dispatch) => {
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTable),
+    };
+
+    fetch(API_URL + "/tables/" + updatedTable.id, options).then(() =>
+      dispatch(editTable(updatedTable))
+    );
+  };
+};
+
+const tablesReducer = (statePart = [], action) => {
+  switch (action.type) {
+    case UPDATE_TABLE:
+      return [...action.payload];
+    case EDIT_TABLE:
+      return statePart.map((table) =>
+        table.id === action.payload.id ? { ...table, ...action.payload } : table
+      );
+    default:
+      return statePart;
+  }
+};
+
 export default tablesReducer;
